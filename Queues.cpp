8c7f100f37b88c40f492c1145c8858402e66785b
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-12 14:44:12 +0700

    Membuat Queues

--- Queues.cpp
+++ Queues.cpp
@@ -6,6 +6,12 @@ class Queues{
         static const int max = 5;
         int FRONT, REAR;
         int queue_array[5];
+    public:
+        Queues(){
+            FRONT = -1;
+            REAR = -1;
+        }
+        
 };
 
 int main(){
