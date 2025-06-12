beef48e8fe7be1db1adb6c354302cb45fd8ce3a4
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-12 15:18:18 +0700

    Pilihan Ketiga

--- Queues.cpp
+++ Queues.cpp
@@ -114,8 +114,11 @@ int main(){
                     q.remove();
                     break;
                 }
+                case '3':{
+                    q.display();
+                    break;
+                }
                 
-
             }
         }
     }
