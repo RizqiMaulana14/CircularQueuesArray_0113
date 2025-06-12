f0bed46187a14ad8414dc3a66a0a8dc4959af4fb
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-06-12 15:05:20 +0700

    Mengecek Dengan Perulangan

--- Queues.cpp
+++ Queues.cpp
@@ -62,6 +62,17 @@ class Queues{
                 cout << "Queue is Empty\n";
                 return;
             }
+
+            cout << "\nELement in the queue are...\n";
+
+            //Jika Front_position <= REAR_position, iterasi dari FRONT hingga REAR
+            if (FRONT_position <= REAR_position){
+                while (FRONT_position <= REAR_position){
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+                cout << endl;
+            }
         }
 };
 
